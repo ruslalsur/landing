58949ba62c747e30af9fa45cc78e47d32a7b013e
rusla <suhrugen@gmail.com>
2021-04-29 22:44:35 +0700

    all component minimazing and optimazing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 import './styles/styles.css'
-import data from './assets/json/data'
-import gbImg from './assets/images/app-gbmoney.png'
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const JsonMinimizerPlugin = require('json-minimizer-webpack-plugin')
+
+const isDevMode = process.env.NODE_ENV === 'development'
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -21,10 +27,10 @@ module.exports = {
     rules: [
       {
         test: /\.s?c?a?ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(webp|png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
@@ -45,21 +51,55 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.json$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+      minify: {
+        collapseWhitespace: !isDevMode,
+      },
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
     new CopyPlugin({
       patterns: [
         {
           from: path.join(__dirname, 'src', 'assets', 'images'),
           to: path.join(__dirname, 'build', 'images'),
         },
+        {
+          from: path.join(__dirname, 'src', 'assets', 'json'),
+          to: path.join(__dirname, 'build', 'json'),
+        },
       ],
     }),
   ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: !isDevMode,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserWebpackPlugin(),
+      new JsonMinimizerPlugin(),
+    ],
+  },
+
+  devtool: 'eval-source-map',
+
+  devServer: {
+    port: 3000,
+    hot: isDevMode,
+    historyApiFallback: true,
+  },
 }
