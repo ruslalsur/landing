1617c8bd6bf1d0b891bab76edcbefe958f7a880b
rusla <suhrugen@gmail.com>
2021-05-03 05:12:24 +0700

    move sass dir to the assert

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './containers/App'
-import './styles/styles.sass'
+import './assets/sass/styles.sass'
 
 render(<App />, document.getElementById('root'))
